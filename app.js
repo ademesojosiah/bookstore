6af8632cfb2630597d59f74f0ab15b39ddb7b1ef
ademesojosiah <josiahademeso@gmail.com>
2023-01-04 23:57:54 +0100

    added book route

--- app.js
+++ app.js
@@ -1,37 +1,32 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const conFig = require('./config/config')
-const connectDb = require('./db/mongoDb')
-const BookValidationMiddleware = require('./validation/books.validator')
+const express = require("express");
+const bodyParser = require("body-parser");
+const conFig = require("./config/config");
+const connectDb = require("./db/mongoDb");
 
+const bookRouter = require("./routes/booksRoute");
 
-
-const app = express()
-
+const app = express();
 
 //connect to mongo db
-connectDb()
+connectDb();
 
 //import body parser middleware
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/',(req,res)=>{
-    res.send('hello bookstore')
-})
-
-
-
-
+app.use("/api/v1/books", bookRouter);
+app.get("/", (req, res) => {
+  res.send("hello bookstore");
+});
 
 //error handler middleware
-app.use((err,req,res,next)=>{
-    console.log(err)
-    const errorStatus =  err.status || 500
+app.use((err, req, res, next) => {
+  console.log(err);
+  const errorStatus = err.status || 500;
 
-    res.status(errorStatus).json({error:err})
-})
+  res.status(errorStatus).json({ error: err });
+});
 
-app.listen(conFig.PORT, ()=>{
-    console.log(`server connected to http://localhost:${conFig.PORT}`);
-})
+app.listen(conFig.PORT, () => {
+  console.log(`server connected to http://localhost:${conFig.PORT}`);
+});
