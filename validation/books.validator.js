8ae45230a50f1cd9641c732926c0f3d209834690
ademesojosiah <josiahademeso@gmail.com>
2023-01-04 23:12:54 +0100

    validation and schema refraction

--- validation/books.validator.js
+++ validation/books.validator.js
@@ -1,44 +1,23 @@
-const Joi = require("joi")
-
+const Joi = require("joi");
 
 const bookValidator = Joi.object({
-    title: Joi.string()
-                .min(5)
-                .max(255)
-                .required()
-                .trim(),
-    shortDescription: Joi.string()
-                        .min(5)
-                        .max(500)
-                        .optional()
-                        .trim(),
-    longDescription: Joi.string()
-                        .min(10)
-                        .optional()
-                        .trim(),
-    year: Joi.number()
-             .integer()
-             .required()
-             .min(2022),
-    price: Joi.number()
-                .min(0)
-                .required(),
-    createdAt: Joi.date()
-                    .default(Date.now),
-    lastUpdatdAt: Joi.date()
-                .default(Date.now)
+  title: Joi.string().min(5).max(255).required().trim(),
+  shortDescription: Joi.string().min(5).max(500).optional().trim(),
+  longDescription: Joi.string().min(10).optional().trim(),
+  year: Joi.number().integer().required().min(2022),
+  price: Joi.number().min(0).required(),
+  createdAt: Joi.date().default(Date.now),
+  lastUpdatdAt: Joi.date().default(Date.now),
+});
 
-                                        
-})
-
-async function BookValidationMiddleware(req,res,next){
-    const bookPayload = req.body
-    try {
-        await bookValidator.validateAsync(bookPayload)
-        next()
-    } catch (error) {
-        next(error.details[0].message)
-    }
+async function BookValidationMiddleware(req, res, next) {
+  const bookPayload = req.body;
+  try {
+    await bookValidator.validateAsync(bookPayload);
+    next();
+  } catch (error) {
+    next(error.details[0].message);
+  }
 }
 
-module.exports = BookValidationMiddleware
+module.exports = BookValidationMiddleware;
