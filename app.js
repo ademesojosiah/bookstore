06109ad8ade886ece697544e45a62b6d9a43bf4c
ademesojosiah <josiahademeso@gmail.com>
2023-01-07 19:47:59 +0100

    initialized rate limiter to limit api calls

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const rateLimit = require('express-rate-limit') 
 const bodyParser = require("body-parser");
 const conFig = require("./config/config");
 const connectDb = require("./db/mongoDb");
@@ -11,9 +12,19 @@ const app = express();
 //connect to mongo db
 connectDb();
 
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+app.use(limiter)
 //import body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 
 app.use("/api/v1/books", bookRouter);
 app.use("/api/v1/authors", authorRouter)
